bc2e0e777480fce6968da5273d1e419077c7f946
Nevio <neviogomez91@gmail.com>
2023-05-04 00:36:30 -0400

    Remove gpt from bufferline

--- nvim/lua/user/barbar.lua
+++ nvim/lua/user/barbar.lua
@@ -19,7 +19,7 @@ require("bufferline").setup({
 
 	-- Excludes buffers from the tabline
 	exclude_ft = { "javascript" },
-	exclude_name = { "package.json" },
+	exclude_name = { "package.json", "__GPT__", "__GPTInput__" },
 
 	-- Enable/disable icons
 	-- if set to 'numbers', will show buffer index in the tabline
